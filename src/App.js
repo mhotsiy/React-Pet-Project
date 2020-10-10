120b178539b5f7cb7aae4bda9df1c371dbbefd60
Marian Hotsiy <marian.hotsiy@justanswer.com>
2020-10-10 14:27:44 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TodoList from './Componets/Todo/TotoList'
+
+
+function App() {
+    return ( 
+		<div className='wrapper'>
+			<h1>React Tutorial</h1>
+			<TodoList />
+		</div>
+	)	
+}
+
+export default App;
